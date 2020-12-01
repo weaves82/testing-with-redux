56ebaffb04698d818ab7fa87561c286bfef2a617
David Weavers <david.weavers@fcvinteractive.com>
2020-11-30 23:28:05 -0800

    Extract setup for enzyme

--- src/Congrats.test.js
+++ src/Congrats.test.js
@@ -1,13 +1,8 @@
 import React from "react";
-import Enzyme, { shallow } from "enzyme";
-import EnzymeAdapter from "@wojtekmaj/enzyme-adapter-react-17";
+import { shallow } from "enzyme";
 
 import { findByTestAttr, checkProps } from "../test/testUtils";
 import Congrats from "./Congrats";
-
-Enzyme.configure({
-  adapter: new EnzymeAdapter(),
-});
 
 const defaultProps = { success: false };
 
