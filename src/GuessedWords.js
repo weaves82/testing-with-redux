0a66bee107e51d71edd69e1ff28e8497b681dd19
David Weavers <david.weavers@fcvinteractive.com>
2020-12-02 23:00:29 -0800

    Tests and code for non-zero guessed

--- src/GuessedWords.js
+++ src/GuessedWords.js
@@ -2,15 +2,38 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const GuessedWords = (props) => {
-  return (
-    <div data-test="component-guessed-words">
-      {props.guessedWords.length === 0 && (
-        <span data-test="guessed-instructions">
-          Try to guess the secret word!
-        </span>
-      )}
-    </div>
-  );
+  let contents;
+  if (props.guessedWords.length === 0) {
+    contents = (
+      <span data-test="guessed-instructions">
+        Try to guess the secret word!
+      </span>
+    );
+  } else {
+    const guessedWordsRows = props.guessedWords.map((word, index) => {
+      return (
+        <tr data-test="guessed-word" key={index}>
+          <td>{word.guessedWord}</td>
+          <td>{word.letterMatchCount}</td>
+        </tr>
+      );
+    });
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>{guessedWordsRows}</tbody>
+        </table>
+      </div>
+    );
+  }
+  return <div data-test="component-guessed-words">{contents}</div>;
 };
 
 GuessedWords.propTypes = {
