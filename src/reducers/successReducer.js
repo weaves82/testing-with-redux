b5d8da9ba8117fa4a510a20ba417a8935ba892ee
David Weavers <david.weavers@fcvinteractive.com>
2020-12-03 16:45:13 -0800

    Add reducers

---
+++ src/reducers/successReducer.js
@@ -0,0 +1,10 @@
+/**
+ * @function successReducer
+ * @params {array} state - Array of guessed words
+ * @params {object} action - action to be reduced
+ * @returns {boolean} - new success state
+ */
+
+export default (state, action) => {
+  return null;
+};
