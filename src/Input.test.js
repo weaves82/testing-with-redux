dbf5da270bdbfdfaac8fb8b0ff6c2687036bf3b2
David Weavers <david.weavers@fcvinteractive.com>
2020-12-05 15:50:20 -0800

    Add Redux prop tests to Input

--- src/Input.test.js
+++ src/Input.test.js
@@ -60,4 +60,16 @@ describe("render", () => {
   });
 });
 
-describe("update state", () => {});
+describe("redux props", () => {
+  test("the success prop is succesfully passed", () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test("the 'guessWord' action creator is a function prop", () => {
+    const wrapper = setup();
+    const guessWordProp = wrapper.instance().props.guessWord;
+    expect(guessWordProp).toBeInstanceOf(Function);
+  });
+});
