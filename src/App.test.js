8b6c169c8f3c73595d035ddd1d66bd94189fa541
David Weavers <david.weavers@fcvinteractive.com>
2020-12-05 16:40:20 -0800

    Add App Component and tests

---
+++ src/App.test.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { storeFactory } from "../test/testUtils";
+
+import App from "./App";
+
+const setup = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  const wrapper = shallow(<App store={store} />)
+    .dive()
+    .dive();
+  return wrapper;
+};
+
+describe("Redux props successfully passed to 'App'", () => {
+  test("has access to 'success' state", () => {
+    const success = true;
+    const wrapper = setup({ success });
+    const successProp = wrapper.instance().props.success;
+    expect(successProp).toBe(success);
+  });
+  test("has access to 'secretWord' state", () => {
+    const secretWord = "party";
+    const wrapper = setup({ secretWord });
+    const secretWordProp = wrapper.instance().props.secretWord;
+    expect(secretWordProp).toBe(secretWord);
+  });
+  test("has access to 'guessedWord' state", () => {
+    const guessedWords = [{ guessedWord: "train", letterMatchCount: 3 }];
+    const wrapper = setup({ guessedWords });
+    const guessedWordsProp = wrapper.instance().props.guessedWords;
+    expect(guessedWordsProp).toBe(guessedWords);
+  });
+  test("the 'getSecretWord' action creator is a function prop", () => {
+    const wrapper = setup();
+    const getSecretWordProp = wrapper.instance().props.getSecretWord;
+    expect(getSecretWordProp).toBeInstanceOf(Function);
+  });
+});
